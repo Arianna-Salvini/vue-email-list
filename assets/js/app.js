d6cad366110b544e973abc9e374a8751865e1933
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-03-19 16:24:35 +0100

    Fix emailGenerator - restore part of function .push accidentally delited before

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,15 +9,16 @@ createApp({
     },
     methods: {
         emailGenerator() {
-            this.emails= []
+            this.emails = []
             for (let i = 0; i < 10; i++) {
                 axios
-                .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then(response => {
-                    // console.log(response.data.response);
-                })
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then(response => {
+                        // console.log(response.data.response);
+                        this.emails.push(response.data.response)
+                    })
             }
-            
+
         }
     },
 
