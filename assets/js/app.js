85ac83a3f7c81e899a08d080344c796ff3cd140c
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-03-19 18:41:25 +0100

    Print the list of emails all together waiting the array == 10
    Add async/await alternative
    
    Add some refctoring

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,19 +4,37 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            emails: []
+            emails: [],
         }
     },
     methods: {
-       async emailGenerator() {
+
+        // emailGenerator() {
+        //     // Svuoto la lista ad ogni generazione
+        //     this.emails = []
+
+        //     // Itero la richiesta per 10 volte
+        //     for (let i = 0; i < 10; i++) {
+        //         axios
+        //             .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        //             .then(response => {
+        //                 // console.log(response.data.response);
+        //                 this.emails.push(response.data.response)
+        //             })
+
+        //     }
+        // },
+
+        ///// Async/await /////
+        async emailGenerator() {
             this.emails = []
+
             for (let i = 0; i < 10; i++) {
                 const response = await axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                        this.emails.push(response.data.response)
-                    }
-        }
-    },
+                this.emails.push(response.data.response)
+            }
 
-    mounted() {
+        },
+
     },
 }).mount('#app')
