54b320637e70346bfa2400f6c6e94275ac41c02f
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-03-19 16:21:08 +0100

    Fix emailGenerator function, at each click the previous list is deleted

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,12 +9,12 @@ createApp({
     },
     methods: {
         emailGenerator() {
+            this.emails= []
             for (let i = 0; i < 10; i++) {
                 axios
                 .get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then(response => {
                     // console.log(response.data.response);
-                    this.emails.push(response.data.response)
                 })
             }
             
