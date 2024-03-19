c63b01bd50fd5265867aeb03f0d9642248cf8556
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-03-19 16:01:52 +0100

    Init - generate emails

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+console.log('Hello API');
+
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+            emails: []
+        }
+    },
+    methods: {
+        emailGenerator() {
+            for (let i = 0; i < 10; i++) {
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => {
+                    console.log(response.data.response);
+                    this.emails.push(response.data.response)
+                })
+            }
+            
+        }
+    },
+
+    mounted() {
+        this.emailGenerator()
+    },
+}).mount('#app')
